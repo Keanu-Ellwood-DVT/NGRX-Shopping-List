3baff3566bb7b9978e9411c8e5aa6ba27581797a
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-27 13:34:03 +0200

    Add ActionReducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,42 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductListReducer } from "src/store/reducers/products.reducer";
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { ActionReducer } from '@ngrx/store';
+
+// Meta-reducers
+// console.log all actions
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state', state);
+    console.log('action', action);
+    return reducer(state, action);
+  };
+}
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ ProductListReducer }),
+
+    StoreModule.forRoot({
+      productlist: ProductListReducer
+    }, {metaReducers: [debug]}),
     BrowserAnimationsModule,
     MatListModule,
-    MatToolbarModule
+
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatExpansionModule,
+    FormsModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
