08d2373f9d717bb858ba802121cec4aed625b711
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-27 13:32:28 +0200

    Add product list + functionality

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,23 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Product } from 'src/store/models/Product';
-import { Store } from '@ngrx/store';
-import { AppState } from 'src/store/models/app-state.model';
-
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { Product } from "src/store/models/Product";
+import { Store } from "@ngrx/store";
+import { AppState } from "src/store/models/app-state.model";
+import { v4 as uuid } from "uuid";
+import {
+  AddItemAction,
+  DeleteItemAction,
+  EditItemAction
+} from "src/store/actions/products.actions";
+import { MatCheckboxChange } from '@angular/material/checkbox';
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
+  productListItems$: Observable<Product[]>;
+  newProduct: Product = { id: "", name: "", complete: false};
+  editedProductName: string;
 
-  productListItems:Observable<Array<Product>>;
+  panelOpenState = false;
+  //checked = false;
 
-
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.productListItems = this.store.select(store => store.productlist);
+    this.productListItems$ = this.store.select(store => store.productlist);
   }
 
+  addProduct() {
+    this.newProduct.id = uuid();
+    this.store.dispatch(new AddItemAction(this.newProduct));
+    this.newProduct = { id: "", name: "", complete: false };
+  }
+
+  deleteProduct(id: string) {
+    this.store.dispatch(new DeleteItemAction({ id }));
+  }
+
+  editProductName(productToUpdate: Product) {
+    this.store.dispatch(
+      new EditItemAction({updatedProduct:{
+        ...productToUpdate,
+        name: this.editedProductName
+      }})
+    );
+    this.editedProductName = "";
+  }
+
+  editProductCheck(productToUpdate: Product, checkedEvent: MatCheckboxChange) {
+    this.store.dispatch(
+      new EditItemAction({updatedProduct:{
+        ...productToUpdate,
+        complete: checkedEvent.checked
+      }})
+    );
+  }
 }
