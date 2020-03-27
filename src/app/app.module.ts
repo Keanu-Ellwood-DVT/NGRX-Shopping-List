0c9b3a82451772d88a0576666eb371552696264e
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-27 08:42:19 +0200

    project deploy to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatListModule } from "@angular/material/list";
+import { AppComponent } from "./app.component";
+import { StoreModule } from "@ngrx/store";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductListReducer } from "src/store/reducers/products.reducer";
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ProductListComponent],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {}),
-    BrowserAnimationsModule
+    StoreModule.forRoot({ ProductListReducer }),
+    BrowserAnimationsModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
