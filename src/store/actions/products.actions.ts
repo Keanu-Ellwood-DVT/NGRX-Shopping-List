3f2d7e1a4d1ca7a4d813ea7c7b78c85d30545330
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-27 13:33:48 +0200

    Add actions and reducers for edit + delete

--- src/store/actions/products.actions.ts
+++ src/store/actions/products.actions.ts
@@ -2,7 +2,9 @@ import { Action } from '@ngrx/store';
 import { Product } from 'src/store/models/Product';
 
 export enum ProductListActionTypes {
-  ADD_PRODUCT = '[PRODUCT-LIST] Add Item',
+  ADD_PRODUCT = '[PRODUCT-LIST] Add Product',
+  DELETE_PRODUCT = '[PRODUCT-LIST] Delete Product',
+  EDIT_PRODUCT = '[PRODUCT-LIST] Edit Product'
 }
 
 export class AddItemAction implements Action {
@@ -11,4 +13,16 @@ export class AddItemAction implements Action {
   constructor(public payload: Product) { }
 }
 
-export type ProductListAction = AddItemAction
+export class DeleteItemAction implements Action {
+  readonly type = ProductListActionTypes.DELETE_PRODUCT
+
+  constructor(public payload: { id: string}) { }
+}
+
+export class EditItemAction implements Action {
+  readonly type = ProductListActionTypes.EDIT_PRODUCT
+
+  constructor(public payload: {updatedProduct: Product}) { }
+}
+
+export type ProductListAction = AddItemAction | DeleteItemAction | EditItemAction
--- src/store/reducers/products.reducer.ts
+++ src/store/reducers/products.reducer.ts
@@ -1,17 +1,45 @@
-import { ProductListActionTypes, ProductListAction } from '../actions/products.actions';
-import { Product } from 'src/store/models/Product';
+import {
+  ProductListActionTypes,
+  ProductListAction
+} from "../actions/products.actions";
+import { Product } from "src/store/models/Product";
 
 const initialState: Array<Product> = [
   {
     id: "1",
-    name: 'Toilet Paper',
+    name: "Toilet Paper",
+    complete: false
+  },
+  {
+    id: "2",
+    name: "Milk",
+    complete: false
+  },
+  {
+    id: "3",
+    name: "Orange Juice",
+    complete: false
+  },
+  {
+    id: "4",
+    name: "Bread",
+    complete: false
   }
 ];
 
-export function ProductListReducer(state: Array<Product> = initialState, action: ProductListAction) {
+export function ProductListReducer(
+  state: Array<Product> = initialState,
+  action: ProductListAction
+) {
   switch (action.type) {
     case ProductListActionTypes.ADD_PRODUCT:
       return [...state, action.payload];
+    case ProductListActionTypes.DELETE_PRODUCT:
+      return state.filter(item => item.id !== action.payload.id);
+    case ProductListActionTypes.EDIT_PRODUCT:
+      return state.map(item => {
+        return item.id === action.payload.updatedProduct.id ? {...action.payload.updatedProduct} : item;
+      });
     default:
       return state;
   }
